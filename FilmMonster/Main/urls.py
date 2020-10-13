71ce94fe3bbd1baea8d02eabc02cfa5055b233d3
Alecksey Shaina <peshkooff00@gmail.com>
2020-10-13 18:22:50 +0300

    Created films view, films page

--- FilmMonster/Main/urls.py
+++ FilmMonster/Main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, films
 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('films/', films, name='films'),
 ]
--- FilmMonster/Main/views.py
+++ FilmMonster/Main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'layout/basic.html')
+
+
+def films(request):
+    return render(request, 'layout/films.html')
